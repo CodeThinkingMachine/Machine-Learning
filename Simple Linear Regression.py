a32e81e9d9f94677b0faa04307ee35ed4881896f
THINKING MACHINE <45174913+CodeThinkingMachine@users.noreply.github.com>
2020-02-03 13:31:14 +0530

    Update Simple Linear Regression.py

--- Simple Linear Regression.py
+++ Simple Linear Regression.py
@@ -12,15 +12,6 @@ plt.show()
 x = dataFrame['Quantity'].values.reshape(-1,1)
 y = dataFrame['Price'].values.reshape(-1,1)
 
-#get a copy of dataset exclude last column
-#x = dataFrame.iloc[:, :-1].values
-
-#get array of dataset in column 1st ...selecting second column values.
-#y = dataFrame.iloc[:, 1].values 
-
-# Splitting the dataset into the Training set and Test set
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_state=0)
-
 # Fitting Simple Linear Regression to the Training set
 regressor = LinearRegression()
 regressor.fit(x, y)
